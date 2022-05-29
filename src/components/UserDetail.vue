64eaac2ad144285d145a85777b7cdfc9ff9d757a
Abdullah Altun <abdullh.altun@gmail.com>
2022-05-29 12:06:04 +0300

    first commit

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wcol-md-6">
+    <h3>Child Component 1</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p>Kullanıcı Adı : {{ name }} </p>
+    <p>Kullanıcı Adı : {{ switchName() }} </p>
+    <button @click="sendToParent" >Veriyi parenta gönder</button>
+  </div>
+</template>
+<script>
+export default {
+  props : ["name"],
+  methods : {
+    switchName(){
+      return this.name.split('').reverse().join('')
+    },
+    sendToParent(){
+      this.$emit()
+    }
+  }
+}
+</script>
+<style scoped>
+  div {
+    background-color: lightcoral;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
